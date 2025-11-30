d7502fc020992e703d239e7261382bf6a23b2269
Ali Choumane <ali.choumane@gmail.com>
2025-11-30 12:44:10 +0100

    Revert "Initialize EncryptedSharedPreferences in main"
    This reverts commit 9706bd9ff6b16372bc0d99b15f0681002267feec.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:encrypt_shared_preferences/provider.dart';
 import 'home.dart';
 
 void main() {
-  EncryptedSharedPreferences.initialize('1111111111111111'); // 16 chars
   runApp(const MyApp());
 }
 
